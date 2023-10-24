93bd5c8ddffd2fdd0bf802fe519af9677956cb9a
Artem Bureev <bureevart@gmail.com>
2023-10-25 00:32:20 +0500

    init proj

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
+import Page1 from './components/pages/Page 1/Page1';
+import Page2 from './components/pages/Page 2/Page2';
 import './App.css';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <nav>
+        <ul>
+          <li>
+            <Link to={'/page1'}>страница 1</Link>
+          </li>
+          <li>
+            <Link to={'/page2'}>страница 2</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="page1" element={<Page1 />} /> 
+        <Route path="page2" element={<Page2 />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/pages/Page 1/Page1.tsx
@@ -0,0 +1,17 @@
+const Page1 = () => {
+
+
+
+    const css = {
+        padding: '10px',
+        border: '5px solid blue'
+    };
+
+
+    return <div style={css}>Это страница 1</div>
+
+
+
+};
+
+export default Page1;
